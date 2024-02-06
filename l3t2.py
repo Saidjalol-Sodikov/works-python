40d8c6a03ed9b2bbe0f7a2d68f17f2053b3fb858
Saidjalol-Sodikov <152029687+Saidjalol-Sodikov@users.noreply.github.com>
2024-02-06 09:09:43 +0500

    Задание 3.2 - выполнено

--- l3t2.py
+++ l3t2.py
@@ -34,3 +34,12 @@ for i in list_1:
         result = i
 
 print(result)    
+
+m = abs(k - list_1[0])  # модуль числа
+number = list_1[0]
+for i in range(1, len(list_1)):
+    if m > abs(list_1[i] - k):
+        m = abs(list_1[i] - k)
+        number = list_1[i]
+print(number)
+
